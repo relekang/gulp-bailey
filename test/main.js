bf1c4fed992cca8bcc1569b9c9946fa466965c5f
Rolf Erik Lekang <me@rolflekang.com>
2014-02-18 22:40:28 -0800

    add a simple test to check if compiling works

---
+++ test/main.js
@@ -0,0 +1,40 @@
+require('mocha');
+var bailey = require('../'),
+    should = require('should'),
+    gutil = require('gulp-util'),
+    fs = require('fs'),
+    path = require('path');
+
+var createFile = function (filepath, contents) {
+    var base = path.dirname(filepath);
+    return new gutil.File({
+      path: filepath,
+      base: base,
+      cwd: path.dirname(base),
+      contents: contents
+    });
+  },
+  testData = function (expected, newPath, done) {
+    return function (newFile) {
+      console.log(newFile);
+      should.exist(newFile);
+      should.exist(newFile.path);
+      String(newFile.contents).should.equal(this.expected);
+    };
+  };
+  
+
+describe('gulp-bailey', function() {
+  describe('bailey()', function() {
+    it('should compile a file', function(done) {
+      var filepath = "test/fixtures/example.bs",
+          contents = new Buffer(fs.readFileSync(filepath)),
+          expected = new Buffer(fs.readFileSync('test/expected/example.js'));
+
+      bailey()
+        .on('error', done)
+        .on('data', testData(expected, filepath.replace('bs', 'js'), done))
+        .write(createFile(filepath, contents));
+    });
+  });
+});
