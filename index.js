56c758b07883950dbe3acc3b14fa717f92149475
Rolf Erik Lekang <me@rolflekang.com>
2014-04-28 01:25:33 -0700

    use bailey.parseString to parse bailey code

---
+++ index.js
@@ -0,0 +1,23 @@
+var es = require('event-stream'),
+    gutil = require('gulp-util'),
+    Buffer = require('buffer').Buffer,
+    path = require('path'),
+    bailey = require('bailey');
+
+module.exports = function (opt) {
+  function modifyFile(file) {
+    if (file.isNull()) return this.emit('data', file); // pass along
+    if (file.isStream()) return this.emit('error', new Error("gulp-bailey: Streaming not supported"));
+
+    var str = file.contents.toString('utf8'),
+        dest = gutil.replaceExtension(file.path, ".js");
+
+    var data = bailey.parseString(str);
+
+    file.contents = new Buffer(data);
+    file.path = dest;
+    this.emit('data', file);
+  }
+
+  return es.through(modifyFile);
+};
